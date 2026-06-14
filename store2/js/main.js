2ae1c3014c51587b6018a2c401fd0f8b7e1b01d5
Yugo Tsuda <tsuda.katachib@gmail.com>
2026-06-14 10:24:23 +0900

    store2を追加

---
+++ store2/js/main.js
@@ -0,0 +1,28 @@
+/*=================================================
+  ハンバーガーメニュー
+  ===================================================*/
+// ハンバーガーメニューのクリックイベント
+document.querySelector(".toggle_btn").addEventListener("click", () => {
+  const header = document.querySelector("#header");
+  if (!header) return;
+  // #headerにopenクラスが存在する場合
+  if (header.classList.contains("open")) {
+    // openクラスを削除
+    // openクラスを削除すると、openクラスのCSSがはずれるため、
+    // メニューが非表示になる
+    header.classList.remove("open");
+
+    // #headerにopenクラスが存在しない場合
+  } else {
+    // openクラスを追加
+    // openクラスを追加すると、openクラスのCSSが適応されるため、
+    // メニューが表示される
+    header.classList.add("open");
+  }
+
+  // メニューが表示されている時に画面をクリックした場合
+  document.querySelector("#mask").addEventListener("click", () => {
+    // openクラスを削除して、メニューを閉じる
+    header.classList.remove("open");
+  });
+});
